8d948a72fc724d14e34639eddc7980577dc1f303
Riya <rkale2@stevens.edu>
2022-04-23 13:11:03 -0400

    Added seed data

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -17,8 +17,8 @@ const getCollectionFn = (collection) => {
 
 /* Now, you can list your collections here: */
 module.exports = {
-  users: getCollectionFn("profile"),
-  users: getCollectionFn("university"),
-  users: getCollectionFn("posts"),
-  users: getCollectionFn("comments"),
+  profiles: getCollectionFn("profile"),
+  universities: getCollectionFn("university"),
+  posts: getCollectionFn("posts"),
+  comments: getCollectionFn("comments"),
 };
