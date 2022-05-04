abb6d2b9577153c66b4f26fdc8b20b0fe2caa4b6
Jefferson Dias <diasjefferson2@gmail.com>
2022-05-04 00:31:57 -0400

    Modified routes

--- routes/universityFinder.js
+++ routes/universityFinder.js
@@ -1,36 +1,31 @@
-const express = require('express');
-const searchData = require('../data/search');
+const express = require("express");
+const searchData = require("../data/search");
 const router = express.Router();
 
-router
-    .get('/search', async (req, res) => {
-        res.render("search/search", { title: "University Finder" });
+router.get("/search", async (req, res) => {
+  res.render("search/search", { title: "University Finder" });
+});
 
+router.get("/search/universities", async (req, res) => {
+  try {
+    const unis = await searchData.getUniversities();
+    res.json(unis);
+  } catch (e) {
+    res.status(404).json({ error: e });
+  }
+});
+
+router.get("/university/:id", async (req, res) => {
+  try {
+    const uni = await searchData.getUniById(req.params.id);
+    res.status(201).render("display/universityInfo", {
+      layout: false,
+      universityData: uni,
     });
-
-
-router
-    .get('/search/universities', async (req, res) => {
-        try {
-            const unis = await searchData.getUniversities();
-            res.json(unis);
-        } catch (e) {
-
-            res.status(404).json({ error: e });
-        }
-    });
-
-router
-    .get('/university/:id', async (req, res) => {
-        try {
-            const uni = await searchData.getUniById(req.params.id);
-            res.send(JSON.stringify(uni));
-            //res.render("search/search", { title: "University Finder" }); fill params acc to that page
-        }
-        catch (e) {
-            res.status(404).json({ error: e });
-        }
-    });
-
+    //res.render("search/search", { title: "University Finder" }); fill params acc to that page
+  } catch (e) {
+    res.status(404).json({ error: e });
+  }
+});
 
 module.exports = router;
