d5df6ddb3998fcef15633042887a1d5bdbb9dab3
chetanGoyal2601 <cgoyal@stevens.edu>
2022-05-08 15:01:14 -0400

    Validation for edit post and comments on client side done

--- public/js/posts/script.js
+++ public/js/posts/script.js
@@ -14,94 +14,51 @@
       }
     });
   }
-  /////
-  let postEditing = document.getElementById("postEditing");
-  let editedPost = document.getElementById("editedPost");
-  if (postEditing) {
-    postEditing.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      if (textValidation(editedPost.value)) {
-        postEditing.submit();
-      } else {
-        alert("Please enter a valid text in the post!");
-        postEditing.reset();
-        newPost.focus();
-      }
-    });
-  }
-  /////
-  /////
-  let f = document.getElementsByTagName("form");
-  //console.log(f[1], f[1].id.substring(0, 11), f[1].length);
-  //let count = 0;
-  //let k = {};
-  for (let i = 0; i < f.length; i++) {
-    //console.log(f[i]);
-    if (f[i].id.substring(0, 11) === "postEditing") {
-      //let postId = formId.substring(11);
-      //k[count] = postId;
-      //let editedPost = document.getElementById("editedPost" + f[i].substring(11));
-      f[i].addEventListener("submit", (event) => {
-        console.log(f[i].substring(11));
-        event.preventDefault();
-        console.log(
-          document.getElementById("editedPost" + f[i].substring(11)).value
-        );
-        if (
-          textValidation(
-            document.getElementById("editedPost" + f[i].substring(11)).value
-          )
-        ) {
-          f[i].submit();
-        } else {
-          alert("Please enter a valid text in the post!");
-          f[i].reset();
-          document.getElementById("editedPost" + f[i].substring(11)).focus();
-        }
-      });
-    }
-  }
-  // let f = document.getElementsByTagName('form')
-  // for (let i of f) {
-  //   if (i) {i.id}
-  // }
 })();
 
-function enableEditing(v1, v2) {
-  const editForm = document.getElementById(v1);
-  const initialPost = document.getElementById(v2);
-  editForm.hidden = false;
-  initialPost.hidden = true;
+function submitEditPost(form, text) {
+  let editForm = document.getElementById(form);
+  let editText = document.getElementById(text);
+  //console.log("Hello1234", editText.value);
+  if (!textValidation(editText.value)) {
+    alert("Enter a valid text to submit the post!");
+    editForm.reset();
+    editForm.focus();
+    return false;
+  } else {
+    console.log("Hello");
+    editForm.submit();
+    return true;
+  }
 }
 
-function enableComment(v3) {
+function enableEditing(v1, v2, v3) {
+  const editForm = document.getElementById(v1);
+  const initialPost = document.getElementById(v2);
+  const editPostSubmitButton = document.getElementById(v3);
+  if (editForm.hidden) {
+    editForm.hidden = false;
+    initialPost.hidden = true;
+    editPostSubmitButton.hidden = false;
+  } else {
+    editForm.hidden = true;
+    initialPost.hidden = false;
+    editPostSubmitButton.hidden = true;
+  }
+}
+
+function enableComment(v3, v4) {
   const commentS = document.getElementById(v3);
+  const commentSubmitButton = document.getElementById(v4);
 
   if (commentS.hidden) {
     commentS.hidden = false;
+    commentSubmitButton.hidden = false;
   } else {
     commentS.hidden = true;
+    commentSubmitButton.hidden = false;
   }
 }
-
-// function commentVerification(v1, v2) {
-//   let commentForm = document.getElementById(v1);
-//   let comment = document.getElementById(v2);
-//   if (commentForm) {
-//     commentForm.addEventListener("submit", (event) => {
-//       event.preventDefault();
-//       //console.log(newPost.value);
-//       if (textValidation(comment.value)) {
-//         commentForm.submit();
-//       } else {
-//         alert("Please enter a valid text in the comment!");
-//         commentForm.reset();
-//         comment.focus();
-//       }
-//     });
-//   }
-// }
 
 //to check if text is string type
 function textValidation(text) {
